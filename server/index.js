085db301dc922485a690e55a8ec57f5abf650239
Ryan Gale <ryanwfgale@gmail.com>
2021-07-15 17:21:40 -0600

    fixes port issue for production

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const dotenv = require('dotenv').config({ path: 'config.env' });
-const express = require('express');
-const session = require('express-session');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const path = require('path');
-const app = module.exports = express();
+const dotenv = require("dotenv").config({ path: "config.env" });
+const express = require("express");
+const session = require("express-session");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = (module.exports = express());
 
 app.use(helmet());
 app.use(cookieParser());
@@ -18,31 +18,39 @@ var sess = {
   secret: process.env.SESSION_SECRET,
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
-    secure: false
+    secure: false,
   },
   resave: false,
   saveUninitialized: true,
-}
+};
 
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1);
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1);
   sess.cookie.secure = true;
 }
 
-app.use(session(sess))
+app.use(session(sess));
 app.use(bodyParser.json());
 
-require(__dirname + '/product.js');
-require(__dirname + '/orders.js');
-require(__dirname + '/admin.js');
-require(__dirname + '/config.js');
+require(__dirname + "/product.js");
+require(__dirname + "/orders.js");
+require(__dirname + "/admin.js");
+require(__dirname + "/config.js");
 
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
 
-app.listen(5000, function() {
-  console.log("Listening on port 5000!");
-});
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("build"));
+
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
+}
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
